4f36d381c253c3e72dd44573428eac98904ccfef
Jim Gallagher <gallagherji@dnb.com>
2016-04-05 16:04:10 +0100

    first release

--- README.md
+++ README.md
@@ -28,59 +28,170 @@ grunt.initConfig({
     options: {
       // Task-specific options go here.
     },
-    your_target: {
+    release: {
       // Target-specific file lists and/or options go here.
     },
   },
 });
 ```
 
-### Options
+This task packages and releases an artifact to a maven repository. It will update the version number in the package.json file to the next development version, and, if this is a git project, it will commit and tag the release.
 
-#### options.separator
-Type: `String`
-Default value: `',  '`
+The version number will have the `patch` number incremented.
 
-A string value that is used to do something with whatever.
+### Overview
 
-#### options.punctuation
-Type: `String`
-Default value: `'.'`
-
-A string value that is used to do something else with whatever else.
-
-### Usage Examples
-
-#### Default Options
-In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
-
-```js
-grunt.initConfig({
-  nexus_release: {
-    options: {},
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
-```
-
-#### Custom Options
-In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
+In your project's Gruntfile, add a section named `nexus_release` to the data object passed into `grunt.initConfig()`.
 
 ```js
 grunt.initConfig({
   nexus_release: {
     options: {
-      separator: ': ',
-      punctuation: ' !!!',
+      groupId: 'com.example',
+      url: '<repository-url>',
     },
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
+    src: [ '**', '!node_modules/**' ]
+  }
+})
 ```
+
+### Options
+
+#### options.groupId
+Type: `String`
+Required
+
+The maven group id to use when deploying and artifact
+
+#### options.artifactId
+Type: `String`
+Default: name found in package.json
+
+The maven artifact id to use when deploying and artifact
+
+#### options.version
+Type: `String`
+Default: version found in package.json or the file specified by options.versionFile
+
+The version to use when deploying to the maven repository
+
+#### options.classifier
+Type: `String`
+Optional
+
+The classifier to use when deploying to the maven repository
+
+#### options.uniqueVersion
+Type: `String`
+Optional
+
+Allow to generate timestamped artifacts.
+
+#### options.mode
+Type: `String`
+Default: minor
+
+The mode passed to semver.inc to determine next development version.
+
+#### options.packaging
+Type: `String`
+Default: zip
+
+The packaging to use when deploying to the maven repository. Will also
+determine the archiving type. As internally the grunt-contrib-compress
+plugin is used to package the artifact, only archiving types supported
+by this module is supported.
+
+#### options.url
+Type: `String`
+Required
+
+The url for the maven repository to deploy to.
+
+#### options.repositoryId
+Type: `String`
+Optional
+
+The repository id of the repository to deploy to. Used for looking up authentication in settings.xml.
+
+#### options.type
+Type: `String`
+Optional
+
+Enables you to choose a different file extension for your artifact besides .zip which is useful when using the Maven WAR-plugin
+
+#### options.injectDestFolder
+Type: `String`
+Optional
+
+Enables you to turn off the injection of destination folder inside your artifact allowing you to choose the structure you want by configuring the compress task.
+
+#### options.destFolder
+Type: `String`
+Optional
+
+Specifies the name of the folder to be injected inside the artifact. If not specified, this will be auto-generated.
+
+#### options.commitPrefix
+Type: `String`
+Optional
+
+Prefix for the commit message when releasing.
+
+#### options.unsecure
+Type: `Boolean`
+Optional
+
+If `true`, runs maven with `-Dmaven.wagon.http.ssl.insecure=true` and `-Dmaven.wagon.http.ssl.allowall=true`
+
+#### options.settingsXml
+Type: `String`
+Optional
+
+Specifies the settings.xml file for `-s` argument.
+
+#### options.optionalParams
+Type: `Array`
+Optional
+
+Appends more optional parameters for `mvn deploy`. [optional parameters list](https://maven.apache.org/plugins/maven-deploy-plugin/deploy-mojo.html)
+
+### Files
+
+Files may be specified using any of the supported [Grunt file mapping formats](http://gruntjs.com/configuring-tasks#files).
+
+### Usage Examples
+
+#### Default Options
+
+'''js
+nexus_release: {
+            options: {
+                groupId: 'com.example.ex',
+                repositoryId: 'nexus'
+            },
+            release: {
+                options: {
+                    goal: 'release',
+                    url: '<>'
+                    destFolder: 'fred',
+                    injectDestFolder : true
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'build/',
+                        src: ['**'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        }
+
+grunt.registerTask('release', [ 'clean', 'test', 'nexus_release:release' ]);
+```
+
+In order to customize the output archive, please look at the documenations for the [grunt-contrib-compress task](https://github.com/gruntjs/grunt-contrib-compress).
 
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
--- tasks/nexus_release.js
+++ tasks/nexus_release.js
@@ -8,43 +8,280 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+var semver = require('semver');
+var fs = require('fs');
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+module.exports = function (grunt) {
 
-  grunt.registerMultiTask('nexus_release', 'release zip artefacts to a Nexus server', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    grunt.registerMultiTask('nexus_release', 'Packages and deploys artifact to maven repo', function (version) {
+        var options = this.options();
+
+        grunt.log.writeln('Starting nexus_release');
+
+        requireOptionProps(options, ['groupId']);
+
+        options.goal = options.goal || this.target;
+        options.commitPrefix = options.commitPrefix || '%s';
+
+        var pkg = grunt.file.readJSON(options.versionFile || 'package.json');
+
+        requireOptionProps(options, ['url']);
+        release(this, pkg, version);
+
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
+    function requireOptionProps(options, props) {
+        var msg = 'Verifying properties ' + grunt.log.wordlist(props) + ' exists in options...';
+        grunt.verbose.write(msg);
+
+        var failProps = props.filter(function (p) {
+            return !options.hasOwnProperty(p);
+        }).map(function (p) {
+            return '"' + p + '"';
+        });
+
+        if (failProps.length === 0) {
+            grunt.verbose.ok();
         } else {
-          return true;
+            grunt.verbose.or.write(msg);
+            grunt.log.error().error('Unable to process task.');
+            throw grunt.util.error('Required options ' + failProps.join(', ') + ' missing.');
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+    }
 
-      // Handle options.
-      src += options.punctuation;
+    function release(task, pkg, version) {
+        var options = task.options({
+            artifactId: pkg.name,
+            packaging: pkg.packaging
+        });
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        options.version = version || pkg.version.substr(0, pkg.version.length - '-SNAPSHOT'.length);
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+        if (options.nextVersion === 'null-SNAPSHOT') {
+            grunt.fail.fatal('Failed to determine next development version ' +
+                'based on version (' + options.version.cyan + ') ');
+        }
+        options.nextVersion = semver.inc(options.version, 'patch') + '-SNAPSHOT';
+        if (options.nextVersion === 'null-SNAPSHOT') {
+            grunt.fail.fatal('Failed to determine next development version ' +
+                'based on version (' + options.version.cyan + ') ');
+        }
+
+        if (!options.file) {
+            options.file = getFileNameBase(options) + '.' + getExtension(options.packaging, options.classifier, options.type);
+        }
+
+        grunt.log.writeln('injectDestFolder:' + options.injectDestFolder + " destFolder: " + options.destFolder);
+        if (typeof options.injectDestFolder === 'undefined' || options.injectDestFolder === true) {
+            var fileNameBase = options.destFolder ? options.destFolder : getFileNameBase(options);
+            task.files = injectDestFolder(fileNameBase, task.files);
+        }
+        grunt.config.set('grunt.maven.commitPrefix', options.commitPrefix);
+
+        options.packaging = getExtension(options.packaging, options.classifier, options.type);
+
+        grunt.config.set('maven.package.options', {archive: options.file, mode: 'zip', extension: options.packaging});
+        grunt.config.set('maven.package.files', task.files);
+        grunt.config.set('maven.deploy-file.options', options);
+        grunt.config.set('maven.install-file.options', options);
+
+        grunt.task.run(
+            'nexus_release:version:' + options.version,
+            'nexus_release:gittag:' + options.artifactId + '-' + options.version,
+            'nexus_release:package',
+            'nexus_release:deploy-file',
+            'nexus_release:version:' + options.nextVersion
+        );
+
+        grunt.task.run('nexus_release:gitcommit');
+
+        grunt.task.run('nexus_release:gitpush');
+
+    }
+
+    grunt.registerTask('nexus_release:deploy-file', function () {
+        var options = grunt.config('maven.deploy-file.options');
+
+        var args = ['deploy:deploy-file'];
+        args.push('-Dfile=' + options.file);
+        args.push('-DgroupId=' + options.groupId);
+        args.push('-DartifactId=' + options.artifactId);
+        args.push('-Dpackaging=' + options.packaging);
+        args.push('-Dversion=' + options.version);
+        if (options.unsecure) {
+            args.push('-Dmaven.wagon.http.ssl.insecure=' + options.unsecure);
+            args.push('-Dmaven.wagon.http.ssl.allowall=' + options.unsecure);
+        }
+        if (options.classifier) {
+            args.push('-Dclassifier=' + options.classifier);
+        }
+        if (options.uniqueVersion === 'true') {
+            args.push('-DuniqueVersion=true');
+        }
+        args.push('-Durl=' + options.url);
+        if (options.repositoryId) {
+            args.push('-DrepositoryId=' + options.repositoryId);
+        }
+        if (options.settingsXml) {
+            // The lack of a space after the -s is critical
+            // otherwise the path will be processed by maven incorrectly.
+            args.push('-s' + options.settingsXml);
+        }
+        // Optional Parameters List :
+        // https://maven.apache.org/plugins/maven-deploy-plugin/deploy-mojo.html
+        if (Array.isArray(options.optionalParams)) {
+            args = args.concat(options.optionalParams);
+        }
+        if (grunt.debug || options.debug) {
+            args.push('-e');
+            args.push('-X');
+        }
+
+        var done = this.async();
+        var msg = 'Deploying to maven...';
+        grunt.verbose.write(msg);
+        grunt.log.debug('Running command "mvn ' + args.join(' ') + '"');
+        grunt.util.spawn({cmd: 'mvn', args: args, opts: {stdio: 'inherit'}}, function (err, result, code) {
+            if (err) {
+                grunt.verbose.or.write(msg);
+                grunt.log.error().error('Failed to deploy to maven');
+            } else {
+                grunt.verbose.ok();
+                grunt.log.writeln('Deployed ' + options.file.cyan + ' to ' + options.url.cyan);
+            }
+            done(err);
+        });
     });
-  });
 
-};
+    grunt.registerTask('nexus_release:package', function () {
+        var compress = require('grunt-contrib-compress/tasks/lib/compress')(grunt);
+        compress.options = grunt.config('maven.package.options');
+        compress.tar(grunt.config('maven.package.files'), this.async());
+
+        renameForKnownPackageTypeArtifacts(compress.options.archive, compress.options.extension);
+    });
+
+    grunt.registerTask('nexus_release:version', 'Bumps version', function (version) {
+        var done = this.async();
+        var commitPrefix = grunt.config('grunt.maven.commitPrefix') || '';
+
+
+        var msg = 'Bumping version to ' + version.cyan + '...';
+        grunt.verbose.write(msg);
+
+        grunt.util.spawn({
+            cmd: 'npm',
+            args: ['version', version, '-m', commitPrefix + '%s']
+        }, function (err, result, code) {
+            if (err) {
+                grunt.verbose.or.write(msg);
+                grunt.log.error().error('Failed to bump version to ' + version.cyan);
+                return done(err);
+            }
+
+            grunt.verbose.ok();
+            grunt.log.writeln('Version bumped to ' + version.cyan);
+
+            return done();
+
+        });
+    });
+
+    grunt.registerTask('nexus_release:gitpush', 'Pushes to git', function () {
+        var done = this.async();
+
+        grunt.verbose.write('Pushing to git');
+
+        gitPush(function (err) {
+            if (err) {
+                grunt.log.error().error('Failed to push new version to remote');
+            } else {
+                grunt.log.writeln('Pushed new version to remote');
+            }
+            done(err);
+        });
+    });
+
+    grunt.registerTask('nexus_release:gitcommit', 'Commits changes to git', function () {
+        var done = this.async();
+
+        grunt.verbose.write('Committing to git');
+
+        gitCommit(function (err) {
+            if (err) {
+                grunt.log.error().error('Failed to commit changes to git');
+            } else {
+                grunt.log.writeln('Committed changes to git');
+            }
+            done(err);
+        });
+    });
+
+    grunt.registerTask('nexus_release:gittag', 'tags git', function (tag) {
+        var done = this.async();
+
+        grunt.verbose.write('Tagging git with ' + tag);
+
+        gitTag(tag, function (err) {
+            if (err) {
+                grunt.log.error().error('Failed to tag new version to remote');
+            } else {
+                grunt.log.writeln('Tagged new version to remote');
+            }
+            done(err);
+        });
+    });
+
+    function gitPush(fn) {
+        grunt.util.spawn({cmd: 'git', args: ['push']}, function (err, result, code) {
+            fn(err);
+        });
+    }
+
+    function gitTag(version, fn) {
+        grunt.util.spawn({
+            cmd: 'git',
+            args: ['tag', '-a', version, '-m', '[nexus_release] release tag ' + version]
+        }, function (err, result, code) {
+            fn(err);
+        });
+    }
+
+    function gitCommit(fn) {
+        grunt.util.spawn({cmd: 'git', args: ['commit','-a','-m','[nexus_release] prepare for next development iteration']}, function (err, result, code) {
+            fn(err);
+        });
+    }
+
+    function getExtension(packaging, classifier, type) {
+        if (classifier === 'javadoc' || classifier === 'sources') {
+            return 'zip';
+        }
+        return type || packaging || 'zip';
+    }
+
+    function renameForKnownPackageTypeArtifacts(fileName, extension) {
+        var newFileName = fileName.replace('zip', extension);
+        try {
+            fs.renameSync(fileName, newFileName);
+            return newFileName;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    function getFileNameBase(options) {
+        return options.artifactId + '-' + options.version +
+            (options.classifier ? '-' + options.classifier : '');
+    }
+
+    function injectDestFolder(targetPath, files) {
+        var path = require('path');
+        files.forEach(function (file) {
+            file.dest = path.join(targetPath, file.dest || '');
+        });
+        return files;
+    }
+
+}
+;
